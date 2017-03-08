35d613237f6389f4f84306d78a7f289fb67e57fc
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-03-08 23:56:52 +0530

    average.py

--- average.py
+++ average.py
@@ -7,18 +7,20 @@ try:
     file.close()
 except:              # is try except like if-else?
     print "Unable to open file"
-sum = 0
-values = 0
-if(len(lines) > 0):
-    for line in lines:
-        value = 0
-        try:
-            value = float(string.strip(line)) # i don't understand this line
-        except ValueError:
-            pass
-        if(value != 0):
-            sum = sum + value
-            values += 1
-    print "Average = %f for %d lines, sum = %f" %(sum/values,values,sum)
-else:
-    print "No lines in the file"
+
+def average(sum,values):
+	sum = 0
+	values = 0
+	if(len(lines) > 0):
+    		for line in lines:
+        		value = 0
+        	try:
+            		value = float(string.strip(line)) # i don't understand this line
+        	except ValueError:
+            		pass
+        		if(value != 0):
+            			sum = sum + value
+            			values += 1
+    		print "Average = %f for %d lines, sum = %f" %(sum/values,values,sum)
+	else:
+    		print "No lines in the file"
