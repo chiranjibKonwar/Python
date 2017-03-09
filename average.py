153283cbb87b4d64ee3d52e566928b15709b4183
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-03-09 12:32:12 +0530

    function added

--- average.py
+++ average.py
@@ -17,20 +17,26 @@ except:              # is try except like if-else?
 
 #KMK lear how to create a function
 
-sum = 0
-values = 0
-if(len(lines) > 0):
-    for line in lines:
-        value = 0
-        try:
-            value = float(string.strip(line)) # i don't understand this line
-        except ValueError:
-            pass
-        if(value != 0):
-            sum = sum + value
-            values += 1
-    print "Average = %f for %d lines, sum = %f" %(sum/values,values,sum)
-else:
-    print "No lines in the file"
+def averageTotal():
+	sum = 0
+	values = 0
+	if(len(lines) > 0):
+   	 for line in lines:
+       		value = 0
+        	try:
+            		value = float(string.strip(line)) # i don't understand this line
+       		except ValueError:
+            		pass
+        	if(value != 0):
+            		sum = sum + value
+            		values += 1
+    	print "Average = %f for %d lines, sum = %f" %(sum/values,values,sum)
+				else:
+    				print "No lines in the file"
+
+averageTotal()
+averageTotal()
+
+
 
 
