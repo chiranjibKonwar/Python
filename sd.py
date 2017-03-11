9cd74d944d9de76760429a1e95ebda44a4a22f14
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-03-11 12:59:14 +0530

    standard Deviation function added

--- sd.py
+++ sd.py
@@ -6,15 +6,18 @@ def standardDeviation(numbers):
 	for numberstring in numbers:
 		value=0
 		squareOfDiff=0
-		sumOfsqDiff=0
+		AvgOfsqOfDiff=0
+		sumOfsquareOfDiff=0
+		sqRoot=0
 		try:
 			value=float(numberstring.strip())
 			squareOfDiff=(value-average(numbers))**2
-			AvgsumOfsqDiff=(sumOfsqDiff+squareOfDiff)/len(numbers)
-			sqRootAvgsumOfsqDiff=AvgsumOfsqDiff**1/2
-					
+			sumOfsquareOfDiff=sumOfsquareOfDiff + squareOfDiff
+			
 		except ValueError:
 			pass
+		AvgOfsqOfDiff=sumOfsquareOfDiff/len(numbers)
+		sqRoot=AvgOfsqOfDiff**1/2
 		standardDeviation=0
-		standardDeviation=sqRootAvgsumOfsqDiff
+		standardDeviation=sqRoot
 		return standardDeviation
