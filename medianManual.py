e9ee4c19c7a5fe8bf19f79b6b61912bbbbad5b8f
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-03-17 12:54:13 +0530

    manual median calculation

---
+++ medianManual.py
@@ -0,0 +1,30 @@
+import sys
+
+
+
+def number():
+
+	fileName=sys.argv[1]
+	file=open(fileName,'r')
+	fileread=file.readlines()
+	file.close()
+	numbers=[]
+	try:
+		for line in fileread:
+			numbers.append(float(line))
+		numbers.sort()
+		print numbers
+		print "Total count of numbers: %d" %(len(numbers))
+		if(len(numbers)%2==0):
+			print "The median of the numbers is: \n"
+			print (numbers[len(numbers)/2] + numbers[len(numbers)/2-1])/2
+		else:
+			print "The median of the numbers is: \n"
+			print numbers[(len(numbers)+1)/2-1]
+	except ValueError:
+		pass
+
+
+number()
+
+
