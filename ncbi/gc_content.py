682abb7c9a059a6c2d2c54a525a7933bd46a2509
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-03-27 11:43:51 +0530

    gc_content calculation with filename as command-line argument

--- ncbi/gc_content.py
+++ ncbi/gc_content.py
@@ -1,4 +1,8 @@
-fh=open("homosapiens.fasta",'r')
+import sys
+
+filename=sys.argv[1]
+
+fh=open(filename,'r')
 
 file=fh.read()
 x=file
@@ -6,26 +10,20 @@ c=0
 a=0
 g=0
 t=0
+
 for x in file:
 	if "C" in x:
-		c+=1
-print "C=%d"%c
-
-for x in file:	
-	if "G" in x:
+		c+=1	
+	elif "G" in x:
 		g+=1
-print "G=%d" %g
-
-for x in file:
-	if "A" in x:
-		a+=1
-print "A=%d"%a
-
-for x in file:	
-	if "T" in x:
+	elif "A" in x:
+		a+=1	
+	elif "T" in x:
 		t+=1
-print "T=%d" %t
+
+print "C=%d, G=%d, A=%d, T=%d" %(c,g,a,t)
 
 gc_content=(g+c)*100/(a+t+g+c)
 
 print "gc_content= %f" %(gc_content)
+
