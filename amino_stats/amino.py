203e9d7fa218c6403d1e4e2bb9039be95976b65c
kishori82 <kishori82@gmail.com>
2017-03-30 12:14:59 -0400

    created the amino acid stats calculator and organization of modules

---
+++ amino_stats/amino.py
@@ -0,0 +1,38 @@
+import sys
+#from fastareader import FastaReader, FastaRecord
+from utilities.fastamodule.fastareader import *
+
+def fprintf(file, fmt, *args):
+   file.write(fmt % args)
+
+def printf(fmt, *args):
+   sys.stdout.write(fmt % args)
+   sys.stdout.flush()
+ 
+def eprintf(fmt, *args):
+   sys.stderr.write(fmt % args)
+   sys.stderr.flush()
+
+
+
+
+if __name__=="__main__":
+
+   filename = sys.argv[1]
+
+   fastaobject = FastaReader(filename)
+
+   for a in fastaobject:
+      print a.name,  len(a.sequence)
+    
+
+   print filename
+   print 'hello'
+
+
+### output should be like
+#  A 10%
+#  B 15%
+#  ....
+#  N means anything
+
