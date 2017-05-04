2f93cd14094274796045e19f4de64f81ecbca093
Kishori M Konwar <kishori82@gmail.com>
2017-05-04 07:25:20 -0400

    fixed the minimum problem

--- best_hit/best_hit.py
+++ best_hit/best_hit.py
@@ -2,28 +2,37 @@ import sys
 
 def inputfile():
 
-	filename = sys.argv[1]
-	fh = open(filename,'r')
-	file = fh.readlines()
-	return file
-	
+    filename = sys.argv[1]
+    fh = open(filename,'r')
+    file = fh.readlines()
+    return file
+    
 
 def file_dict():
-	file = inputfile()
-	d = {}	
-	for line in file:
-		rows = line.split('\t')
-		d = {rows[0]:rows[10]}
-		print d
-		
+    file = inputfile()
+    d = {}    
+
+    for line in file:
+       rows = line.split('\t')
+
+      
+       # if this is the first time the query is encounter or 
+       # the new one is even smaller then the one already in 'd' then  keep it
+       if not rows[0] in d or  d[rows[0]] > float(rows[10]):
+           d[rows[0]] = float(rows[10])
+
+    #now print the results out
+    for key, value in d.iteritems():
+         print key, value
 
 if __name__=="__main__":
 
-	inputfile()
-	file_dict()
+    inputfile()
+
+    file_dict()
 
 
-	
+    
 
 
 
