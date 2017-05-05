d939f52b813ac36f68e921fa41e80b2279dd90a4
chiranjibKonwar <chiranjib.konwar@gmail.com>
2017-05-05 13:45:01 +0530

    added target sequence to best hit result

--- best_hit/best_hit.py
+++ best_hit/best_hit.py
@@ -14,16 +14,18 @@ def file_dict():
 
     for line in file:
        rows = line.split('\t')
-
       
        # if this is the first time the query is encounter or 
        # the new one is even smaller then the one already in 'd' then  keep it
+       d[rows[0]] = []
        if not rows[0] in d or  d[rows[0]] > float(rows[10]):
-           d[rows[0]] = float(rows[10])
+           d[rows[0]] = (rows[1], float(rows[10]))
 
     #now print the results out
     for key, value in d.iteritems():
-         print key, value
+	 sys.stdout=open("best_hits.txt","a")
+	 print key, value
+	 sys.stdout.close()
 
 if __name__=="__main__":
 
